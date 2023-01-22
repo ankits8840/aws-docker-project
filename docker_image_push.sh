aac94dfd4690d90ec7805d984c78d993b6161d74
ankit <ankit@gmail.com>
2023-01-22 06:49:25 -0500

    final commit

--- docker_image_push.sh
+++ docker_image_push.sh
@@ -1,6 +1,6 @@
 #!  /bin/bash
 profile="default"
-region="us-west-2"
+region="ap-south-1"
 buildTag="shellscript-demo:latest"
 
 echo "$1"
@@ -20,7 +20,7 @@ if [[ -n "$3" ]]; then
 fi
 
 
-aws ecr get-login-password --region "${region}" --profile "${profile}" | docker login --username AWS --password-stdin "120717539064.dkr.ecr.${region}.amazonaws.com"
+aws ecr get-login-password --region "${region}" --profile "${profile}" | docker login --username AWS --password-stdin "073810531838.dkr.ecr.${region}.amazonaws.com"
 docker build -t "${buildTag}" .
-docker tag "${buildTag}" "120717539064.dkr.ecr.us-west-2.amazonaws.com/${buildTag}"
-docker push "120717539064.dkr.ecr.us-west-2.amazonaws.com/${buildTag}"
+docker tag "${buildTag}" "073810531838.dkr.ecr.ap-south-1.amazonaws.com/${buildTag}"
+docker push "073810531838.dkr.ecr.ap-south-1.amazonaws.com/${buildTag}"
